b4cb08558e67d0dbde0b92e4d93f6470d1d1cd97
cballevre <celestin.ballevre@protonmail.com>
2024-06-24 19:06:01 +0200

    feat(Announcement): Improve schema

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,21 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ButtonLinkButton extends Schema.Component {
+  collectionName: 'components_button_link_buttons';
+  info: {
+    displayName: 'LinkButton';
+    icon: 'exit';
+  };
+  attributes: {
+    label: Attribute.String & Attribute.Required;
+    link: Attribute.String & Attribute.Required;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'button.link-button': ButtonLinkButton;
+    }
+  }
 }

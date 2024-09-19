e6e034963f462c0ecd7b03d3ec33dc28543e61f7
cballevre <celestin.ballevre@protonmail.com>
2024-09-19 12:03:11 +0200

    fix: Manage emoji inside mysql database

--- config/database.ts
+++ config/database.ts
@@ -12,6 +12,7 @@ export default ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
+        charset: env("DATABASE_CHARSET", 'utf8mb4'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
